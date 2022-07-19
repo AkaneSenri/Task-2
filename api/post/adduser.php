e0d37d95430d4ee270ea2c265fba531444aca024
AkaneSenri <97027085+AkaneSenri@users.noreply.github.com>
2022-07-19 13:37:31 +0300

    Add files via upload

---
+++ api/post/adduser.php
@@ -0,0 +1,31 @@
+<?php 
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../../config/Database.php';
+include_once '../../models/Users.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$user = new Users($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$user->email = $data->email;
+$user->full_name = $data->full_name;
+$user->gender = $data->gender;
+$user->status = $data->status;
+
+if($user->addUser()) {
+    echo json_encode(
+      array('message' => 'User was created')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'Error')
+    );
+}
